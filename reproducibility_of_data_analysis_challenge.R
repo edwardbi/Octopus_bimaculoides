e4650dd661590fc6c8af744154e037343a6fcf6d
Dehua Bi <dbi@uchicago.edu>
2019-09-12 13:07:39 -0500

    The reproducibility main code --Edward Bi

---
+++ reproducibility_of_data_analysis_challenge.R
@@ -0,0 +1,25 @@
+rm(list=ls())
+
+library(tidyverse)
+library(RMKdiscrete)
+library(ggplot2)
+
+# set work dir to your dir
+# setwd("C:/Users/edwar/Documents/MBL2019/BSD-QBio5/tutorials/reproducibility")
+
+# Load data
+# input the data
+bug_data <- read_csv("./data/cole_arthropod_data_1946.csv")
+
+# Plot the Poisson distribution with the same mean as spider counts
+# 1) calculate mean of spider count
+mean_spider <- mean(bug_data$C_count_of_boards_with_k_spiders)
+# 2) plot the poisson
+spider_poisson <- dpois(0:17,lambda = 1/mean_spider)
+all_spider = sum(bug_data$C_count_of_boards_with_k_spiders)
+p_spider <- ggplot(bug_data,aes(x=k_number_of_arthropods))
+p_spider <- p_spider + geom_line(aes(y=spider_poisson),color="darkred") + 
+  geom_point(aes(y=C_count_of_boards_with_k_spiders/all_spider),color="darkblue") +
+  scale_y_continuous(sec.axis = sec_axis(~.*all_spider, name = "spider_data"))
+p_spider 
+
